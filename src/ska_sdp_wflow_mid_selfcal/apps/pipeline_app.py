60b40fb15d1ea511933b4b50deb2c6dabf558cc1
Vincent Morello <vmorello@gmail.com>
2023-04-11 18:42:46 +0100

    HIP-532: Remove slurm mode placeholder We will implement it when the time comes

--- src/ska_sdp_wflow_mid_selfcal/apps/pipeline_app.py
+++ src/ska_sdp_wflow_mid_selfcal/apps/pipeline_app.py
@@ -1,7 +1,6 @@
 import argparse
 import logging
 import os
-import sys
 
 from ska_sdp_wflow_mid_selfcal import selfcal_pipeline
 from ska_sdp_wflow_mid_selfcal.directory_creation import (
@@ -29,14 +28,6 @@ def parse_args() -> argparse.Namespace:
         ),
     )
     parser.add_argument(
-        "--slurm",
-        action="store_true",
-        help=(
-            "Generate a slurm script out of the given command line and "
-            "submit it to the queue."
-        ),
-    )
-    parser.add_argument(
         "--base-outdir",
         type=str,
         default=os.getcwd(),
@@ -53,45 +44,11 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def generate_slurm_script(argv: list[str]) -> str:
-    """
-    Generate a slurm script wrapping the command line with which this app was
-    called, minus the options related to SLURM.
-    `argv` is the list of command-line arguments as returned by `sys.argv`.
-    """
-    command_line = " ".join(argv).replace("--slurm", "")
-    script_lines = [
-        "#SLURM Header",
-        "module load whatever",
-        "conda activate some_env",
-        command_line,
-    ]
-    script = "\n".join(script_lines)
-    return script
-
-
-def run_slurm_mode(argv: list[str]) -> None:
-    """
-    Run the program in SLURM mode. That is, generate a slurm script wrapping
-    the command line with which this app was called, minus the options related
-    to SLURM, and print script to stdout.
-    `argv` is the list of command-line arguments as returned by `sys.argv`.
-    """
-    log.info("Running in SLURM mode")
-    script = generate_slurm_script(argv)
-    print(script)
-
-
 def main():
     """
     Entry point for the selfcal pipeline app.
     """
     args = parse_args()
-
-    if args.slurm:
-        run_slurm_mode(sys.argv)
-        return
-
     outdir = create_pipeline_output_subdirectory(args.base_outdir)
     logfile_path = os.path.join(outdir, "logfile.txt")
     setup_logging(logfile_path)

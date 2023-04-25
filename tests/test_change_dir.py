ce22ab7e71851bf3e6b94d754a59c909f58ed56b
Vincent Morello <vmorello@gmail.com>
2023-04-25 09:34:32 +0100

    HIP-521: Delete obvious test

--- tests/test_change_dir.py
+++ tests/test_change_dir.py
@@ -18,20 +18,6 @@ def test_changedir():
         assert os.getcwd() == cwd_before
 
 
-def test_changedir_is_robust_to_exception():
-    """
-    Check that ChangeDir does change back if an exception is raised within it.
-    """
-    cwd_before = os.getcwd()
-    with tempfile.TemporaryDirectory() as tempdir_name:
-        try:
-            with ChangeDir(tempdir_name):
-                raise RuntimeError()
-        except RuntimeError:
-            pass
-        assert os.getcwd() == cwd_before
-
-
 def test_changedir_raises_on_nonexistent_directory():
     """
     Check that FileNotFound is raised when using ChangeDir on a path that does

46f5d46ea8b9b36de495a99112e8667b34f55132
Vincent Morello <vmorello@gmail.com>
2023-07-03 18:40:16 +0100

    HIP-603: Add test for rendering wsclean command with just mpirun modifier

--- tests/test_command_utils.py
+++ tests/test_command_utils.py
@@ -92,6 +92,46 @@ def test_wsclean_command_rendering_with_singularity_exec(
     assert render_command_with_modifiers(wsclean_command, [mod]) == expected
 
 
+def test_wsclean_command_rendering_with_mpirun(
+    wsclean_command: WSCleanCommand,
+):
+    """
+    Test that WSCleanCommand with Mpirun modifier as expected.
+    """
+    mpirun = Mpirun(8)
+    expected = [
+        "mpirun",
+        "-np",
+        "8",
+        "-npernode",
+        "1",
+        "wsclean-mp",
+        "-join-channels",
+        "-multiscale",
+        "-channels-out",
+        "8",
+        "-deconvolution-channels",
+        "1",
+        "-fit-spectral-pol",
+        "1",
+        "-name",
+        "final",
+        "-niter",
+        "42",
+        "-scale",
+        "1asec",
+        "-size",
+        "8192",
+        "4096",
+        "-weight",
+        "briggs",
+        "-0.5",
+        "/path/to/data1.ms",
+        "/path/to/data2.ms",
+    ]
+    assert render_command_with_modifiers(wsclean_command, [mpirun]) == expected
+
+
 def test_wsclean_command_rendering_with_singularity_exec_and_mpirun(
     wsclean_command: WSCleanCommand,
 ):
